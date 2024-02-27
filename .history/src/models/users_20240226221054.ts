ec6a5109d7a524c4cb8cb630ef2d9e45140c3743
HenrryWith2Rs <50603179+HenrryWith2Rs@users.noreply.github.com>
2024-02-26 22:14:18 -0500

    revert

---
+++ .history/src/models/users_20240226221054.ts
@@ -0,0 +1,32 @@
+import { Schema, model } from 'mongoose';
+import { UserProps } from '../types/types';
+import Joi from 'joi';
+
+// Validation schema
+export const UserSchemaValidate = Joi.object({
+  userId: Joi.string().required(),
+  username: Joi.string().required(),
+  firstName: Joi.string().required(),
+  lastName: Joi.string().required(),
+  email: Joi.string().required(),
+  password: Joi.string().required(),
+});
+
+// User Config
+const UserSchema = new Schema({
+  userId: { type: String, unique: true, required: true },
+  username: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  salt: { type: String },
+  role: {
+    type: String,
+    enum: ['Administrator', 'Moderator', 'User'],
+    default: 'User',
+  },
+});
+
+// Create model
+export const UserModel = model<UserProps>('User', UserSchema);
